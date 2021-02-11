ad96010b80136d62a65b5aefd3050a613101b18e
Aloisio Vieira <aloisio.vieira@gmail.com>
2021-02-10 23:23:54 -0300

    project 06 is complete

--- 06-todo-rest-api-h2-containerized/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ 06-todo-rest-api-h2-containerized/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,16 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class RestfulWebServicesApplicationTests {
+class RestfulWebServicesApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
+		
 	}
 
 }
