cdaf55808c191a4be40d67a5238c2208a080f4b0
Aloisio Vieira <aloisio.vieira@gmail.com>
2021-02-10 22:16:49 -0300

    project 05 is complete

---
+++ 05-spring-boot-react-full-stack-h2/frontend/todo-app/build/precache-manifest.36e155267038bd4cf97ab3cea25c7c76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3fef771d0b8434c41a25",
+    "url": "/static/js/main.fe7510d3.chunk.js"
+  },
+  {
+    "revision": "3be61595d9a1137a984f",
+    "url": "/static/js/2.3f64e426.chunk.js"
+  },
+  {
+    "revision": "3fef771d0b8434c41a25",
+    "url": "/static/css/main.40229a1d.chunk.css"
+  },
+  {
+    "revision": "2e16b402c4fc5f7305e390b7cac247c5",
+    "url": "/index.html"
+  }
+];
---
+++ 05-spring-boot-react-full-stack-h2/restful-web-services/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests2.java
@@ -0,0 +1,12 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+
+class RestfulWebServicesApplicationTests2 {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
